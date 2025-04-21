083ddef71d215543e99853ff03766020f093dfe4
Leonidnrv <leonidnrv@gmail.com>
2025-04-21 18:11:02 +0300

    commit 2

---
+++ src/ClientHandler.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable{
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>; //va stoca toti clientii. Cand un client va trimit un mesaj, vom face loop pe aceasta lista si vom trimite acelasi mesaj la fiecare din clientii conectati. Este statica pentru ca vrem sa apartina clasei, nu fiecarui obiect.
+    private Socket socket; //va fi responsabila pentru stabilirea conexiunii dintre client si server
+    private BufferedReader bufferedReader; //va fi folosita pentru a citi datele trimise de la client
+    private BufferedWriter bufferedWriter; //va fi folosita pentru a trimite datele catre client
+    private String clientUserName; //va distinge fiecare client
+
+    public ClientHandler(Socket socket){
+        try{
+            //Fiecare socket va avea un inputstream de unde va primi date si un outputstream unde va trimite date
+            this.socket = socket;
+            //stream de caractere (in Java observam streamurile de caractere prin cuvantul Writer). OutputStreamWriter = charcater stream / socket.getOutputStream = byte stream
+            //vom folosi buffer pentru eficienta
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.clientUserName = bufferedReader.readLine(); //programul se va opri aici si va astepta sa introducem numele noului client.
+            clientHandlers.add(this); //dupa ce s-a scris numele utilizatorului, adaugam la ArrayList obiectul curent (this)
+            broadcastMessage("SERVER: " + clientUserName + " has entered the chat!")
+        }catch(IOException e){
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    @Override
+    public void run() {
+        String messageFromClient;
+        while(socket.isConnected()){
+            try{
+                //acest thread se va bloca aici si va astepta un mesaj de la clienti. Pentru ca va fi un thread separat, nu va in curca aplicatia principala
+                messageFromClient = bufferedReader.readLine(); //datele de la alti clienti vor ajunge in buffer
+                broadcastMessage(messageFromClient); //vom distribui mesajul catre toti clienti din ArrayList
+            }catch(IOException e){
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+        }
+    }
+}
--- src/Server.java
+++ src/Server.java
@@ -17,8 +17,29 @@ public class Server {
                 Socket socket = serverSocket.accept(); //programul se va bloca aici pana cand un client se va conecta.
                 // un obiect tip socket va fi returnat si va fi folosit la comunicarea cu clientul.
                 System.out.println("Un nou client s-a conectat!");
-                ClientHandler clientHandler = new ClientHandler(); //fiecare obiect din aceasta clasa va fi responsabil cu comunicarea cu clientul. Implementeaza interfata Runnable.
+                ClientHandler clientHandler = new ClientHandler(socket); //fiecare obiect din aceasta clasa va fi responsabil cu comunicarea cu clientul. Implementeaza interfata Runnable.
+                Thread thread = new Thread(clientHandler);
+                thread.start();
             }
+        }catch(IOException e){
+
         }
+    }
+
+    //Daca va aparea o eroare vom inchide serverul cu aceasta metoda
+    public void closeServerSocket(){
+        try{
+            if(serverSocket != null){
+                serverSocket.close();
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        ServerSocket serverSocket = new ServerSocket(4444); //clientii vor putea sa se conecteze cu adresa si portul setat aici
+        Server server = new Server(serverSocket);
+        server.startServer();
     }
 }
